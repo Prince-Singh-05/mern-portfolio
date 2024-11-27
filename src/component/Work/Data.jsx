7d3224337a40751a46cbaf03b5714a03973e0645
Prince-Singh-05 <luciefur6393473659@gmail.com>
2024-11-27 17:33:31 +0530

    Work section

---
+++ src/component/Work/Data.jsx
@@ -0,0 +1,63 @@
+import img1 from "../../assets/work1.jpg";
+import img2 from "../../assets/work2.png";
+import img3 from "../../assets/work3.png";
+import img4 from "../../assets/work4.jpg";
+import img5 from "../../assets/work5.png";
+
+export const projectData = [
+	{
+		id: 1,
+		image: img1,
+		title: "Study Notion",
+		category: "MERN",
+		url: "",
+		github: "https://github.com/Prince-Singh-05/Study-Notion-EdTech",
+	},
+	{
+		id: 2,
+		image: img2,
+		title: "Chit Chat",
+		category: "MERN",
+		url: "https://chit-chat-a8zd.onrender.com",
+		github: "https://github.com/Prince-Singh-05/chit-chat",
+	},
+	{
+		id: 3,
+		image: img3,
+		title: "Personal Portfolio",
+		category: "Design, Frontend",
+		url: "",
+		github: "https://github.com/Prince-Singh-05/mern-portfolio",
+	},
+	{
+		id: 4,
+		image: img4,
+		title: "Discord Landing Page",
+		category: "Frontend",
+		url: "https://prince-singh-05.github.io/Discord_Clone/",
+		github: "https://github.com/Prince-Singh-05/Discord_Clone",
+	},
+	{
+		id: 5,
+		image: img5,
+		title: "Pen Studio",
+		category: "Design",
+		url: "https://pen-studio.vercel.app/",
+		github: "https://github.com/Prince-Singh-05/pen-studio",
+	},
+];
+
+export const projectsNav = [
+	{
+		name: "all",
+	},
+	{
+		name: "frontend",
+	},
+	{
+		name: "design",
+	},
+	{
+		name: "mern",
+	},
+];
